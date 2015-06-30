5ae849c55a045221c663cbe7591366a53b1fc5b6
Price Bryan <pbryan422@gmail.com>
2015-06-30 18:19:49 -0500

    The Start of Something New
    Let's do this.

---
+++ Standalone_Calculator.cpp
@@ -0,0 +1,18 @@
+#iostream <iostream>
+using namespace std;
+
+int x = 0; // Number that holds the value of the users choice for FPS rating
+
+void fps(); // The function to determine the score the game is rated on Frames Per Second
+
+int main()
+{
+  fps()
+  
+  cin << x
+}
+
+fps()
+{
+  cout << "1: May be capped to 30 FPS << endl; << "May be capped to 60 FPS"" << endl;
+}
