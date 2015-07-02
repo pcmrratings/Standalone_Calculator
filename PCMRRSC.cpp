3194f5fcd1fa3b053f13de2a88c242da0140721a
taserman21 <pbryan422@gmail.com>
2015-07-01 20:35:04 -0500

    Tweaking

--- PCMRRSC.cpp
+++ PCMRRSC.cpp
@@ -9,13 +9,13 @@ void fps(); // The function to determine the score the game is rated on Frames P
 int main()
 {
   fps();
-  
+
   cin >> x;
-  
+
   return 0;
 }
 
 void fps()
 {
-  cout << "1: May be capped to 30 FPS" << endl << "2: May be capped to 60 FPS" << "60 FPS capped" << "60 FPS capped, potentially limitless" << endl;
+  cout << "1: May be capped to 30 FPS" << endl << "2: May be capped to 60 FPS" << endl << "3: 60 FPS capped" << endl << "4: 60 FPS capped, potentially limitless" << endl << "5: limitless" << endl;
 }
