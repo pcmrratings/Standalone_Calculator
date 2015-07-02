7d6bc8d8805124528e278c1f88e882d970e8d6ec
taserman21 <pbryan422@gmail.com>
2015-07-02 18:19:43 -0500

    Major update, FPS successful

--- PCMRRSC.cpp
+++ PCMRRSC.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-int fpsScore(0); // Number that holds the value of the users choice for FPS rating
+char yn='x'; // Value of users choice y/n
 
+//SCORES//
+int scoreFPS(0); // Holds the value of the user's choice for FPS rating
+
+int scoreResolution(0); // Holds the value of the user's choice for Resolution rating
+
+int scoreOptimization(0); // Holds the value of the user's choice for Optimization rating
+
+int scoreModSupport(0); // Holds the value of the user's choice for Mod Support rating
+
+int scoreServers(0); // Holds the value of the user's choice for Servers rating
+
+int scoreDLC(0); // Holds the value of the user's choice for DLC rating
+
+int scoreGlitches(0); // Holds the value of the user's choice for Glitches rating
+
+int scoreSettings(0); // Holds the value of the user's choice for Settings rating
+
+int scoreControls(0); // Holds the value of the user's choice for Controls rating
+
+int scoreTotal(0); // Holds the value of the total score of the game
+
+//SCORE CALCULATORS//
 void fps(); // The function to determine the score the game is rated on Frames Per Second
+
+void total(); // The function to determine the total score the game is rated dependent on users input
+
+void rating() // Displays ratings so far and total
+{
+    cout << "|FPS:         |      " << scoreFPS << endl;
+    cout << "|Resolution:  |      " << scoreResolution << endl;
+    cout << "|Optimization:|      " << scoreOptimization << endl;
+    cout << "|Mod Support  |      " << scoreModSupport << endl;
+    cout << "|Servers:     |      " << scoreServers << endl;
+    cout << "|DLC:         |      " << scoreDLC << endl;
+    cout << "|Glitches:    |      " << scoreGlitches << endl;
+    cout << "|Settings:    |      " << scoreSettings << endl;
+    cout << "|Controls:    |      " << scoreControls << endl;
+    cout << "|Total:       |      " << scoreTotal << endl;
+}
+
+int reactionFPS(); // Determines whether users choice was correct and reacts accordingly
 
 int main()
 {
   fps();
+
+  reactionFPS();
+
+  rating();
 
   return 0;
 }
@@ -17,12 +62,26 @@ void fps()
 {
   cout << "1: May be capped to 30 FPS" << endl << "2: May be capped to 60 FPS" << endl << "3: 60 FPS capped" << endl << "4: 60 FPS capped, potentially limitless" << endl << "5: limitless" << endl;
 
-  cin >> fpsScore;
+  cin >> scoreFPS;
 
-  if (fpsScore == 1)
-  {
-    cout << "You chose 1" << endl;
-  }
+  cout << "You chose " << scoreFPS << endl << "Is this correct? y/n" << endl;
 
+  cin >> yn;
+
+}
+
+int reactionFPS()
+{
+    if(yn=='y')
+    {
+        return 0;
+    }
+
+    else if (yn=='n')
+    {
+        system("reset");
+
+        fps();
+    }
 }
 
