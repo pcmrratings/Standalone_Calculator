7634393fcf262b0c73b3f32c36dbf660e3cb48cd
taserman21 <pbryan422@gmail.com>
2015-07-01 23:30:40 -0500

    Last minute (tiny) change

--- PCMRRSC.cpp
+++ PCMRRSC.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int x(0); // Number that holds the value of the users choice for FPS rating
+int fpsScore(0); // Number that holds the value of the users choice for FPS rating
 
 void fps(); // The function to determine the score the game is rated on Frames Per Second
 
@@ -10,12 +10,19 @@ int main()
 {
   fps();
 
-  cin >> x;
-
   return 0;
 }
 
 void fps()
 {
   cout << "1: May be capped to 30 FPS" << endl << "2: May be capped to 60 FPS" << endl << "3: 60 FPS capped" << endl << "4: 60 FPS capped, potentially limitless" << endl << "5: limitless" << endl;
+
+  cin >> fpsScore;
+
+  if (fpsScore == 1)
+  {
+    cout << "You chose 1" << endl;
+  }
+
 }
+
