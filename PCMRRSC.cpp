5f5b66199d0a1e104c07a783fb085660f61c2e7a
taserman21 <pbryan422@gmail.com>
2015-07-02 21:27:15 -0500

    Nit-picking

--- PCMRRSC.cpp
+++ PCMRRSC.cpp
@@ -28,22 +28,38 @@ int scoreTotal(0); // Holds the value of the total score of the game
 
 //SCORE CALCULATORS//
 
-void fps();
+void fps(); // Function that determines the score of FPS
+
+void resolution(); // Function that determines the score of Resolution
+
+void optimization();
+
+void modSupport();
+
+void servers();
+
+void dlc();
+
+void  glitches();
+
+void settings();
+
+void controls();
 
 void total(); // The function to determine the total score the game is rated dependent on users input
 
 void rating() // Displays ratings so far and total
 {
-    cout << "|FPS:         |" << scoreFPS << "|" << endl;
-    cout << "|Resolution:  |" << scoreResolution << "|" << endl;
-    cout << "|Optimization:|" << scoreOptimization << "|" << endl;
-    cout << "|Mod Support  |" << scoreModSupport << "|" << endl;
-    cout << "|Servers:     |" << scoreServers << "|" << endl;
-    cout << "|DLC:         |" << scoreDLC << "|" << endl;
-    cout << "|Glitches:    |" << scoreGlitches << "|" << endl;
-    cout << "|Settings:    |" << scoreSettings << "|" << endl;
-    cout << "|Controls:    |" << scoreControls << "|" << endl;
-    cout << "|Total:       |" << scoreTotal << "|" << endl;
+        cout << "|FPS:         |" << scoreFPS << "|" << endl;
+            cout << "|Resolution:  |" << scoreResolution << "|" << endl;
+                cout << "|Optimization:|" << scoreOptimization << "|" << endl;
+                    cout << "|Mod Support  |" << scoreModSupport << "|" << endl;
+                        cout << "|Servers:     |" << scoreServers << "|" << endl;
+                            cout << "|DLC:         |" << scoreDLC << "|" << endl;
+                                cout << "|Glitches:    |" << scoreGlitches << "|" << endl;
+                                    cout << "|Settings:    |" << scoreSettings << "|" << endl;
+                                        cout << "|Controls:    |" << scoreControls << "|" << endl;
+                                            cout << "|Total:       |" << scoreTotal << "|" << endl;
 }
 
 int reactionFPS(); // Determines whether users choice was correct and reacts accordingly
