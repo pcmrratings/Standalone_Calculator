d072592a5674559fc577b16a9359ca9ff249650c
Price Bryan <pbryan422@gmail.com>
2015-07-06 16:47:58 +0000

    Reorganizing

---
+++ Linux/scores.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//SCORES//
+int scoreFPS(0); // Holds the value of the user's choice for FPS rating
+
+int scoreResolution(0); // Holds the value of the user's choice for Resolution rating
+
+int scoreOptimization(0); // Holds the value of the user's choice for Optimization rating
+
+int scoreModSupport(0); // Holds the value of the user's choice for Mod Support rating
+
+int scoreServers(0); // Holds the value of the user's choice for Servers rating
+
+int scoreDLC(0); // Holds the value of the user's choice for DLC rating
+
+int scoreGlitches(0); // Holds the value of the user's choice for Glitches rating
+
+int scoreSettings(0); // Holds the value of the user's choice for Settings rating
+
+int scoreControls(0); // Holds the value of the user's choice for Controls rating
+
+int scoreTotal(0); // Holds the value of the total score of the game
+
